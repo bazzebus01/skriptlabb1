29257787e3d9926d5e800a2eb27e840daabd705f
bazzebus <bazze91@hotmail.com>
2026-01-20 09:26:47 +0100

    first upload
    made the menu printing logics,
    need to add funktions

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import modules
+
+running = 1
+while running == 1:
+    print("1. få ett film förslag")
+    print("2. gissa film")
+    print("3. visa alla filmer")
+    print("4. avsluta program")
+    userInput = input("skriv in menyalternativ:")
+
